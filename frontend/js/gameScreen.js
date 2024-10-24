bef4c8b62dfe9917ea0ed1696864d93d288f0048
Herick José <hjf@academico.ufpb.br>
2024-10-24 11:05:05 -0300

    Adiciona novos avatares e modifica arquivos frontend

--- frontend/js/gameScreen.js
+++ frontend/js/gameScreen.js
@@ -3,7 +3,26 @@
 */
 const linkButton        = document.getElementById('link_button');
 const searchSong        = document.getElementById('search_song');
+const exitRoomBtn       = [...document.getElementsByClassName('btn_exit')]
+const startBtn          = [...document.getElementsByClassName('btn_start')][0]
+const restartBtn        = document.getElementById('btn_restart')
+const nextRoundBtn      = [...document.getElementsByClassName('btn_next_round')][0]
 
+// divs for each match moment
+    // round_box
+const waitingForRound   = document.getElementById('waiting_for')
+const whilePlayingRound = document.getElementById('while_playing')
+const betweenRounds     = document.getElementById('between_rounds')
+const rankingBoxRounds  = document.getElementById('ranking_box')
+    // songs_box
+const waitingForSong    = document.getElementById('waiting_for_song')
+const didntFoundSong    = document.getElementById('didnt_found')
+const whilePlayingSong  = document.getElementById('while_playing_song')
+const betweenRoundsSong = document.getElementById('between_rounds_song')
+const rankingBoxSong    = document.getElementById('ranking_box_song')
+
+// song touch
+const songs             = [...document.getElementsByClassName('song_found')]
 
 function copyLink(code){
     var link = "http://localhost:5500/frontend/index.html?id=" + code;
@@ -63,7 +82,67 @@ function playSong(link, image, artist, song, isRight){
         symbol.style.color = '#F04949'
         document.getElementById('isRight').innerHTML = 'WROOOONG!';
     }
- }
+}
+
+const cleanBoxes = () => {
+    didntFoundSong.style.display = 'none'
+    waitingForRound.style.display = 'none'
+    waitingForSong.style.display = 'none'
+    whilePlayingRound.style.display = 'none'
+    whilePlayingSong.style.display = 'none'
+    betweenRounds.style.display = 'none'
+    betweenRoundsSong.style.display = 'none'
+    rankingBoxRounds.style.display = 'none'
+    rankingBoxSong.style.display = 'none'
+}
+
+exitRoomBtn.forEach((el) => {
+    el.addEventListener('click', () => {
+        console.log("exiting room...")
+    })
+}) 
+startBtn.addEventListener('click', () => {
+    // windows logics
+    if(waitingForRound){
+        cleanBoxes()
+        
+        whilePlayingRound.style.display = 'flex'
+        whilePlayingSong.style.display = 'flex'
+    }
+})
+nextRoundBtn.addEventListener('click', () => {
+    // windows logics
+    if(betweenRounds){
+        cleanBoxes()
+
+        whilePlayingRound.style.display = 'flex'
+        whilePlayingSong.style.display = 'flex'
+    }
+})
+restartBtn.addEventListener('click', () => {
+    // windows logics
+    if(whilePlayingRound){
+        cleanBoxes()
+        
+        whilePlayingRound.style.display = 'flex'
+        whilePlayingSong.style.display = 'flex'
+    }
+    // runTimeBar(15)
+})
+
+songs.forEach((el) => {
+    el.addEventListener('click', () => {
+        let selectedList = [...document.getElementsByClassName('song_selected')]
+
+        if(selectedList.length > 0){
+            selectedList.forEach((item) =>{
+                item.classList.remove('song_selected')
+            })
+        }
+        
+        el.classList.add('song_selected')
+    })
+})
 
 
 link = "https://cdnt-preview.dzcdn.net/api/1/1/4/5/9/0/459c235b39ef8badd04c5ef181f58c24.mp3?hdnea=exp=1729648847~acl=/api/1/1/4/5/9/0/459c235b39ef8badd04c5ef181f58c24.mp3*~data=user_id=0,application_id=42~hmac=41b71fd4dd3bd0338c3b5510b46e60d7f0b81499ef1351e18bf49e1ca4f21e47"
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,12 +4,19 @@
 
 const nickname          = document.getElementById('nickname_input');
 const roomCode          = document.getElementById('roomCode_input');
-const password          = document.getElementById('password_input');
+const password          = document.getElementById('create_password');
 const goto_enterRoom    = document.getElementById('goto_enterRoom');
 const goto_createRoom   = document.getElementById('goto_createRoom');
 const enterRoom         = document.getElementById('enterRoom');
 const createRoom        = document.getElementById('createRoom');
 const flags             = [...document.getElementsByClassName('flag')]
+const userImg           = [...document.getElementsByClassName('user_image')]
+const changeImg         = [...document.getElementsByClassName('change_image')]
+const images            = [...document.getElementById('images').children]
+const selectImages      = document.getElementById('selection_images')
+
+const maxPlayersInput   = document.getElementById('max_players')
+const nRoundsInput   = document.getElementById('rounds')
 
 // parametros e funcao para autofill
 const inputCode = document.getElementById('roomCode_input');
@@ -20,6 +27,8 @@ const id = urlParams.get('id');
 function fillCode(id){
     if(id !== ""){
         // tratar ids maiores que 6
+
+        
         inputCode.value = id;
     }
 }
@@ -29,8 +38,9 @@ fillCode(id);
 function removeFlagSelected(){
     // Remove qual bandeira estiver escolhida
     let selected = document.getElementsByClassName("selected")
-    if(selected != null){
+    if(selected.length != 0){
         selected = selected[0]
+        // console.log(selected.classList)
         selected.classList.remove('selected')
     }
 }
@@ -69,6 +79,35 @@ enterRoom.addEventListener('click', () => {
     /*
         Redirects the user to the 'room' page, if the password of the room is correct.
     */
+   
+    let nickNameRegex = /^[a-zA-Z0-9]+$/;
+    let passwordRegex = /^[a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?/\\|~`-]{6,10}$/
+    let roomCodeRegex = /^[A-Z0-9]{6}$/
+    
+    if(!nickNameRegex.test(nickname.value)){
+        nickname.value = ""
+        // "voltar" a tela
+        document.getElementById('enter-room').style.display = 'none'
+        document.getElementById('credentials').style.display = 'flex'
+        
+        alert("Choose a nickname with letters and numbers only!")
+        return;
+    }
+    if(!passwordRegex.test(password.value)){
+        console.log(password.value)
+        password.value = ""
+        alert("Fill the password with letters, numbers and valid simbols only!")
+        return;
+    }
+    if(!roomCodeRegex.test(roomCode.value)){
+        roomCode.value = ""
+        
+        document.getElementById('enter-room').style.display = 'none'
+        document.getElementById('credentials').style.display = 'flex'
+
+        alert("Invalid Code!")
+        return;
+    }
     
     let route = '';
     fetch(route, {
@@ -103,6 +142,49 @@ createRoom.addEventListener('click', () => {
         Redirects the user to the new 'room' page, if all the data are correct.
     */
 
+    // garantir que a entrada numerica nao esteja fora do intervalo definido
+    if(maxPlayersInput.value == "" || nRoundsInput.value == "" || password.value == ""){
+        alert("Preencha todos os campos")
+        return
+    }
+
+    let max_players = parseInt(maxPlayersInput.value)   // 2 - 20
+    let num_rounds = parseInt(nRoundsInput.value)       // 3 - 15
+
+    if(max_players > 20){
+        max_players = 20;
+    }else if(max_players < 2){
+        max_players = 2;
+    }
+    maxPlayersInput.value = max_players
+
+    if(num_rounds > 15){
+        num_rounds = 15;
+    }else if(num_rounds < 3){
+        num_rounds = 3;
+    }
+    nRoundsInput.value = num_rounds
+
+    // teste de validade do nickname e da senha
+    let nickNameRegex = /^[a-zA-Z0-9]+$/;
+    let passwordRegex = /^[a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?/\\|~`-]{6,10}$/
+    
+    if(!nickNameRegex.test(nickname.value)){
+        nickname.value = ""
+        // "voltar" a tela
+        document.getElementById('create-room').style.display = 'none'
+        document.getElementById('credentials').style.display = 'flex'
+        
+        alert("Choose a nickname with letters and numbers only!")
+        return;
+    }
+    if(!passwordRegex.test(password.value)){
+        password.value = ""
+        alert("Choose a password with letters, numbers and valid simbols only!")
+        return;
+    }
+
+    selectImages.style.visibility = "hidden"
     let route = '';
     fetch(route, {
         method: 'POST',
@@ -130,3 +212,19 @@ createRoom.addEventListener('click', () => {
         console.error('Error:', error);
     });
 });
+
+changeImg.forEach((el) => {
+    el.addEventListener('click', ()=>{
+        console.log(selectImages)
+        console.log(selectImages.style.display)
+        selectImages.style.visibility = "visible"
+    })
+})
+
+images.forEach((el, i) => {
+    el.addEventListener('click', ()=>{
+        userImg[0].style.backgroundImage = `url(/SongGuesser/assets/avatars/${i+1}.png)`
+        userImg[1].style.backgroundImage = `url(/SongGuesser/assets/avatars/${i+1}.png)`
+        selectImages.style.visibility = "hidden";
+    })
+})
