df0d6ebfc44b001b987566d75bd15ade4dbdad38
Herick José <125290993+Herickjf@users.noreply.github.com>
2024-10-23 21:08:22 -0300

    Add files via upload

--- frontend/js/gameScreen.js
+++ frontend/js/gameScreen.js
@@ -18,7 +18,7 @@ function copyLink(code){
     alert(`Código ${code} copiado para área de transferência!`);
 }
 linkButton.addEventListener('click', () =>{
-    copyLink('123456');
+    copyLink('12345678');
 })
 
 
@@ -47,9 +47,6 @@ function playSong(link, image, artist, song, isRight){
 
 
     let album_image = document.getElementById('album_image');
-    // album_image.style.width = 100;
-    // album_image.style.height = 100;
-    // album_image.style.backgroundImage = `url(${image})`;
 
     let artist_name = document.getElementById('artist_info');
     let song_name = document.getElementById('track_info');
@@ -59,13 +56,15 @@ function playSong(link, image, artist, song, isRight){
     let symbol = document.getElementById('symbol_isRight');
     if(isRight){
         symbol.classList = 'fa fa-check-circle';
+        symbol.style.color = '#56F956'
         document.getElementById('isRight').innerHTML = 'RIIIIGHT!';
     } else{
         symbol.classList = 'fa fa-times-circle';
+        symbol.style.color = '#F04949'
         document.getElementById('isRight').innerHTML = 'WROOOONG!';
     }
  }
 
 
 link = "https://cdnt-preview.dzcdn.net/api/1/1/4/5/9/0/459c235b39ef8badd04c5ef181f58c24.mp3?hdnea=exp=1729648847~acl=/api/1/1/4/5/9/0/459c235b39ef8badd04c5ef181f58c24.mp3*~data=user_id=0,application_id=42~hmac=41b71fd4dd3bd0338c3b5510b46e60d7f0b81499ef1351e18bf49e1ca4f21e47"
-playSong(link, "https:\/\/api.deezer.com\/artist\/75491\/image", "Lady Gaga", "Die with a smile", false);
+playSong(link, "https:\/\/api.deezer.com\/artist\/75491\/image", "Lady Gaga", "Die with a smile", true);
